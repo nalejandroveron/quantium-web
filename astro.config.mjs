4228def4c1108984387e7c5d173803631fdf8204
Nahuel Veron <nahuelav96@gmail.com>
2025-10-23 17:11:49 -0300

    chore: Fix css

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "./",
+  outDir: "dist",
+
   vite: {
     plugins: [tailwindcss()],
   },
